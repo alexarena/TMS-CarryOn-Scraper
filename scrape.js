6de46f69f474f30096e86073ed64f307ca2ccf90
Alex Arena <im@alexarena.com>
2017-06-01 12:27:47 -0400

    ignore weird airlines with no dimension requirements

--- scrape.js
+++ scrape.js
@@ -4,6 +4,9 @@ const fs = require('fs')
 const cheerio = require('cheerio')
 const tableparser = require('cheerio-tableparser')
 
+//Some options...
+//const ignoreUnboundedDimensions = true
+
 let url = 'http://travel-made-simple.com/carry-on-size-chart/'
 request(url, (err, res, body)=> {
   const $ = cheerio.load(body)
@@ -12,7 +15,7 @@ request(url, (err, res, body)=> {
 
   let airlines = []
   for(let i=1; i<table[0].length; i++){
-    airlines.push({
+    let tmp = {
       name: table[0][i],
       imperial: {
         height: +table[1][i], //Inches
@@ -28,7 +31,14 @@ request(url, (err, res, body)=> {
         linear: Math.ceil((+table[4][i]*2.54)),
         weight: Math.ceil((+table[5][i]*0.453592)) //Pounds -> Kilo
       }
-    })
+    }
+
+    if((tmp.imperial.height !== 0 &&
+       tmp.imperial.width !== 0 &&
+       tmp.imperial.depth !== 0)
+     ){
+       airlines.push(tmp)
+     }
   }
   writeOut(airlines)
 })
